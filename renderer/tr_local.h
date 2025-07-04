2d05b81d486cf2e66d0127e519b0e58fff6e9f3f
Daniel Gibson <metalcaedes@gmail.com>
2025-07-04 09:00:04 +0200

    Revert "#6606. Now tonemap happens during GUI overlays rendering and only affects "3d game" output."
    This reverts commit 4cf82e59dd1750090af1b9c6861bb2e35c660b36.

--- renderer/tr_local.h
+++ renderer/tr_local.h
@@ -649,10 +649,6 @@ struct drawSurfsCommand_t : emptyCommand_t {
 	viewDef_t	*viewDef;
 };
 
-struct tonemapCommand_t : emptyCommand_t {
-	bool forceOutputToBlack;
-};
-
 struct drawLightgemCommand_t : drawSurfsCommand_t {
 	byte *dataBuffer;
 };
